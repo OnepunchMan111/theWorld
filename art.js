1566e2f0e6e56edc9f1c3421cb8f4271be08093a
onepunch_Man <1581723002@qq.com>
2020-10-24 22:11:55 +0800

    delete art

--- art.js
+++
@@ -1,8 +0,0 @@
-const template = require('art-template')
-const path = require('path')
-const views = path.join(__dirname,'index.art')
-const html = template(views,{
-    name:'张三',
-    age:20
-})
-console.log(html)
